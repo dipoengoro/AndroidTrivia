2f5b8c6d6a8626529f401d1b76786e9911dbd251
dipoengoro <dipoengoro@outlook.com>
2022-04-02 12:38:23 +0700

    Phase 3

--- app/src/main/java/id/dipoengoro/androidtrivia/fragments/GameFragment.kt
+++ app/src/main/java/id/dipoengoro/androidtrivia/fragments/GameFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import id.dipoengoro.androidtrivia.R
 import id.dipoengoro.androidtrivia.databinding.FragmentGameBinding
@@ -15,29 +14,54 @@ class GameFragment : Fragment() {
     data class Question(
         val text: String,
         val answers: List<String>
-        )
+    )
 
     private val questions: MutableList<Question> = mutableListOf(
-        Question(text = "What is Android Jetpack?",
-            answers = listOf("all of these", "tools", "documentation", "libraries")),
-        Question(text = "Base class for Layout?",
-            answers = listOf("ViewGroup", "ViewSet", "ViewCollection", "ViewRoot")),
-        Question(text = "Layout for complex Screens?",
-            answers = listOf("ConstraintLayout", "GridLayout", "LinearLayout", "FrameLayout")),
-        Question(text = "Pushing structured data into a Layout?",
-            answers = listOf("Data Binding", "Data Pushing", "Set Text", "OnClick")),
-        Question(text = "Inflate layout in fragments?",
-            answers = listOf("onCreateView", "onViewCreated", "onCreateLayout", "onInflateLayout")),
-        Question(text = "Build system for Android?",
-            answers = listOf("Gradle", "Graddle", "Grodle", "Groyle")),
-        Question(text = "Android vector format?",
-            answers = listOf("VectorDrawable", "AndroidVectorDrawable", "DrawableVector", "AndroidVector")),
-        Question(text = "Android Navigation Component?",
-            answers = listOf("NavController", "NavCentral", "NavMaster", "NavSwitcher")),
-        Question(text = "Registers app with launcher?",
-            answers = listOf("intent-filter", "app-registry", "launcher-registry", "app-launcher")),
-        Question(text = "Mark a layout for Data Binding?",
-            answers = listOf("<layout>", "<binding>", "<data-binding>", "<dbinding>"))
+        Question(
+            text = "What is Android Jetpack?",
+            answers = listOf("all of these", "tools", "documentation", "libraries")
+        ),
+        Question(
+            text = "Base class for Layout?",
+            answers = listOf("ViewGroup", "ViewSet", "ViewCollection", "ViewRoot")
+        ),
+        Question(
+            text = "Layout for complex Screens?",
+            answers = listOf("ConstraintLayout", "GridLayout", "LinearLayout", "FrameLayout")
+        ),
+        Question(
+            text = "Pushing structured data into a Layout?",
+            answers = listOf("Data Binding", "Data Pushing", "Set Text", "OnClick")
+        ),
+        Question(
+            text = "Inflate layout in fragments?",
+            answers = listOf("onCreateView", "onViewCreated", "onCreateLayout", "onInflateLayout")
+        ),
+        Question(
+            text = "Build system for Android?",
+            answers = listOf("Gradle", "Graddle", "Grodle", "Groyle")
+        ),
+        Question(
+            text = "Android vector format?",
+            answers = listOf(
+                "VectorDrawable",
+                "AndroidVectorDrawable",
+                "DrawableVector",
+                "AndroidVector"
+            )
+        ),
+        Question(
+            text = "Android Navigation Component?",
+            answers = listOf("NavController", "NavCentral", "NavMaster", "NavSwitcher")
+        ),
+        Question(
+            text = "Registers app with launcher?",
+            answers = listOf("intent-filter", "app-registry", "launcher-registry", "app-launcher")
+        ),
+        Question(
+            text = "Mark a layout for Data Binding?",
+            answers = listOf("<layout>", "<binding>", "<data-binding>", "<dbinding>")
+        )
     )
 
     lateinit var currentQuestion: Question
@@ -74,10 +98,17 @@ class GameFragment : Fragment() {
                             setQuestion()
                             binding.invalidateAll()
                         } else {
-                            it.findNavController().navigate(R.id.action_gameFragment_to_gameWonFragment)
+                            it.findNavController().navigate(
+                                GameFragmentDirections.actionGameFragmentToGameWonFragment(
+                                    numQuestions,
+                                    questionIndex
+                                )
+                            )
                         }
                     } else {
-                        it.findNavController().navigate(R.id.action_gameFragment_to_gameOverFragment)
+                        it.findNavController().navigate(
+                            GameFragmentDirections.actionGameFragmentToGameOverFragment()
+                        )
                     }
                 }
             }
@@ -89,7 +120,8 @@ class GameFragment : Fragment() {
         currentQuestion = questions[questionIndex]
         answers = currentQuestion.answers.toMutableList()
         answers.shuffle()
-        (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.title_android_trivia_question, questionIndex + 1, numQuestions)
+        (activity as AppCompatActivity).supportActionBar?.title =
+            getString(R.string.title_android_trivia_question, questionIndex + 1, numQuestions)
     }
 
     private fun randomizeQuestions() {
--- app/src/main/java/id/dipoengoro/androidtrivia/fragments/GameWonFragment.kt
+++ app/src/main/java/id/dipoengoro/androidtrivia/fragments/GameWonFragment.kt
@@ -1,9 +1,10 @@
 package id.dipoengoro.androidtrivia.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import id.dipoengoro.androidtrivia.R
@@ -11,6 +12,7 @@ import id.dipoengoro.androidtrivia.databinding.FragmentGameWonBinding
 
 class GameWonFragment : Fragment() {
     private lateinit var binding: FragmentGameWonBinding
+    private lateinit var args: GameWonFragmentArgs
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +23,28 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_gameWonFragment_to_gameFragment)
         )
+        args = GameWonFragmentArgs.fromBundle(requireArguments())
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu, menu)
+        if (getShareIntent().resolveActivity(requireActivity().packageManager) == null)
+            menu.findItem(R.id.share).isVisible = false
+    }
+
+
+    private fun getShareIntent(): Intent = ShareCompat.IntentBuilder(requireActivity())
+        .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+        .setType("text/plain")
+        .intent
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.share -> startActivity(getShareIntent())
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
