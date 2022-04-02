b41b368c2ae76d46e1a53f8ba98a5e905f66cc11
dipoengoro <dipoengoro@outlook.com>
2022-04-02 11:50:56 +0700

    Phase 2

--- app/src/main/java/id/dipoengoro/androidtrivia/fragments/TitleFragment.kt
+++ app/src/main/java/id/dipoengoro/androidtrivia/fragments/TitleFragment.kt
@@ -1,11 +1,11 @@
 package id.dipoengoro.androidtrivia.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import id.dipoengoro.androidtrivia.R
 import id.dipoengoro.androidtrivia.databinding.FragmentTitleBinding
 
@@ -22,6 +22,16 @@ class TitleFragment : Fragment() {
         binding.playButton.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         )
+        setHasOptionsMenu(true)
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =
+        NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
 }
